5c20072b7aa1655fd3cb07185b026a74dec93fe4
James Goodwin <james.goodwin@gocanvas.com>
2021-02-08 16:03:03 -0500

    animal and breed dropdowns from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
 
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Puck" animal="Cat" breed="Domestic Shorthair" />
-      <Pet name="Rocket" animal="Dog" breed="Corgi" />
-      <Pet name="Cassie" animal="Dog" breed="Airedale Terrier" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
